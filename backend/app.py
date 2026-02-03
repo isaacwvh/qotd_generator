a3e5326a1f228aab91e91cbc1729104efc758d6d
isaacwvh <isaacwvh@gmail.com>
2026-02-03 00:53:37 -0800

    minor changes

--- backend/app.py
+++ backend/app.py
@@ -10,17 +10,18 @@ quotes = [
     "Discipline is choosing what you want most over what you want now.",
     "Start before you are ready; clarity comes from doing.",
     "Be proud of how far you have come, and excited for what comes next.",
-    "Your future is built from what you do today, not what you plan tomorrow.",
-    "Courage is not the absence of fear; it is moving forward with it.",
-    "A little progress every day adds up to big change.",
-    "You are allowed to be a work in progress and still be worthy of love.",
-    "Focus on what you can control, and let the rest be noise.",
-    "Make decisions as the person you want to become.",
-    "Imperfection is beauty, madness is genius and it is better to be absolutely ridiculous than absolutely boring.",
+    # "Your future is built from what you do today, not what you plan tomorrow.",
+    # "Courage is not the absence of fear; it is moving forward with it.",
+    # "A little progress every day adds up to big change.",
+    # "You are allowed to be a work in progress and still be worthy of love.",
+    # "Focus on what you can control, and let the rest be noise.",
+    # "Make decisions as the person you want to become.",
+    # "Imperfection is beauty, madness is genius and it is better to be absolutely ridiculous than absolutely boring.",
     "Without music, life would be a mistake.",
     "without drake, life would be a mistake",
     "Everything you can imagine is real.",
     "Life isn't about finding yourself. Life is about creating yourself"
+    "CAREFUL, you might miss it! https://www.speedtest.net/"
 ]
 
 
